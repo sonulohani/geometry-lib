4d8853396eed3ee3732c275ef0a44a2338e8ad2a
Sonu Lohani <sonulohani@gmail.com>
2018-09-17 20:29:40 +0530

    Completed Point class; Added unit tests for point class

--- geometry-lib/Point.h
+++ geometry-lib/Point.h
@@ -6,93 +6,150 @@
 #include <type_traits>
 #include <exception>
 #include <ostream>
+#include <initializer_list>
 
 namespace slohani {
 
-	template <typename T = int>
+	template <typename T>
 	class Point {
 	public:
 		explicit Point() = default;
 
 		explicit Point(T xPos, T yPos) {
-			m_xPos = xPos;
-			m_yPos = yPos;
+			mXPos = xPos;
+			mYPos = yPos;
 		}
 
 		template <typename U = T,
 			std::enable_if_t<std::is_integral_v<U>, U>* = nullptr>
-			inline T isNull() const {
-			return (m_xPos == 0 && m_yPos == 0);
+			constexpr inline T isNull() const {
+			return (mXPos == 0 && mYPos == 0);
 		}
 
 		template <typename U = T,
 			std::enable_if_t<std::is_floating_point_v<U>, U>* = nullptr>
-			inline T isNull() const {
-			return (std::abs(m_xPos) < std::numeric_limits<U>::epsilon() &&
-				std::abs(m_yPos) < std::numeric_limits<U>::epsilon());
+			constexpr inline T isNull() const {
+			return (std::abs(mXPos) < std::numeric_limits<U>::epsilon() &&
+				std::abs(mYPos) < std::numeric_limits<U>::epsilon());
 		}
 
-		inline void setX(T xPos) { m_xPos = xPos; }
+		inline void setX(T xPos) { mXPos = xPos; }
 
-		inline void setY(T yPos) { m_yPos = yPos; }
+		inline void setY(T yPos) { mYPos = yPos; }
 
-		inline T getX() const { return m_xPos; }
+		inline T getX() const { return mXPos; }
 
-		inline T getY() const { return m_yPos; }
+		inline T getY() const { return mYPos; }
 
-		Point<T>& operator+(const Point<T> point) {
-			m_xPos += point.getX();
-			m_yPos += point.getY();
+		constexpr Point<T> operator+(const Point<T>& point) {
+			Point<T> returnedPoint = *this;
+			returnedPoint.mXPos += point.getX();
+			returnedPoint.mYPos += point.getY();
+			return returnedPoint;
+		}
+
+		constexpr Point<T> operator+(T value) {
+			Point<T> returnedPoint = *this;
+			returnedPoint.mXPos += value;
+			returnedPoint.mYPos += value;
+			return returnedPoint;
+		}
+
+		constexpr Point<T>& operator+=(const Point<T>& point) {
+			mXPos += point.getX();
+			mYPos += point.getY();
 			return *this;
 		}
 
-		Point<T>& operator+(T value) {
-			m_xPos += value;
-			m_yPos += value;
+		constexpr Point<T>& operator+=(T value) {
+			mXPos += value;
+			mYPos += value;
 			return *this;
 		}
 
-		Point<T>& operator-(const Point<T> point) {
-			m_xPos -= point.getX();
-			m_yPos -= point.getY();
+		constexpr Point<T> operator-(const Point<T>& point) {
+			Point<T> returnedPoint = *this;
+			returnedPoint.mXPos -= point.getX();
+			returnedPoint.mYPos -= point.getY();
+			return returnedPoint;
+		}
+
+		constexpr Point<T> operator-(T value) {
+			Point<T> returnedPoint = *this;
+			returnedPoint.mXPos -= value;
+			returnedPoint.mYPos -= value;
+			return returnedPoint;
+		}
+
+		constexpr Point<T>& operator-=(const Point<T>& point) {
+			mXPos -= point.getX();
+			mYPos -= point.getY();
 			return *this;
 		}
 
-		Point<T>& operator-(T value) {
-			m_xPos -= value;
-			m_yPos -= value;
+		constexpr Point<T>& operator-=(T value) {
+			mXPos -= value;
+			mYPos -= value;
 			return *this;
 		}
 
 		template <typename U = T,
 			std::enable_if_t<std::is_integral_v<U>, U>* = nullptr>
-			Point<T>& operator*(T factor) {
+			constexpr Point<T> operator*(T factor) {
 			if (factor == 1)
 			{
 				return *this;
 			}
 
-			m_xPos *= factor;
-			m_yPos *= factor;
-			return *this;
+			Point<T> returnedPoint = *this;
+			returnedPoint.mXPos *= factor;
+			returnedPoint.mYPos *= factor;
+			return returnedPoint;
 		}
 
 		template <typename U = T,
 			std::enable_if_t<std::is_floating_point_v<U>, U>* = nullptr>
-			Point<T>& operator*(T factor) {
+			constexpr Point<T> operator*(T factor) {
 			if (factor == 1.0)
 			{
 				return *this;
 			}
 
-			m_xPos *= factor;
-			m_yPos *= factor;
+			Point<T> returnedPoint = *this;
+			returnedPoint.mXPos *= factor;
+			returnedPoint.mYPos *= factor;
+			return returnedPoint;
+		}
+
+		template <typename U = T,
+			std::enable_if_t<std::is_integral_v<U>, U>* = nullptr>
+			constexpr Point<T>& operator*=(T factor) {
+			if (factor == 1)
+			{
+				return *this;
+			}
+
+			mXPos *= factor;
+			mYPos *= factor;
+			return *this;
+		}
+
+		template <typename U = T,
+			std::enable_if_t<std::is_floating_point_v<U>, U>* = nullptr>
+			constexpr Point<T>& operator*=(T factor) {
+			if (factor == 1.0)
+			{
+				return *this;
+			}
+
+			mXPos *= factor;
+			mYPos *= factor;
 			return *this;
 		}
 
 		template <typename U = T,
 			std::enable_if_t<std::is_integral_v<U>, U>* = nullptr>
-			Point<T>& operator/(T divisor) {
+			constexpr Point<T> operator/(T divisor) {
 			if (divisor == 0) {
 				throw std::exception("Cannot divide by zero!");
 			}
@@ -101,14 +158,15 @@ namespace slohani {
 				return *this;
 			}
 
-			m_xPos /= divisor;
-			m_yPos /= divisor;
-			return *this;
+			Point<T> returnedPoint = *this;
+			returnedPoint.mXPos /= divisor;
+			returnedPoint.mYPos /= divisor;
+			return returnedPoint;
 		}
 
 		template <typename U = T,
 			std::enable_if_t<std::is_floating_point_v<U>, U>* = nullptr>
-			Point<T>& operator/(T divisor) {
+			constexpr Point<T> operator/(T divisor) {
 			if (divisor == 0.0) {
 				throw std::exception("Cannot divide by zero!");
 			}
@@ -117,31 +175,81 @@ namespace slohani {
 				return *this;
 			}
 
-			m_xPos /= divisor;
-			m_yPos /= divisor;
+			Point<T> returnedPoint = *this;
+			returnedPoint.mXPos /= divisor;
+			returnedPoint.mYPos /= divisor;
+			return returnedPoint;
+		}
+
+		template <typename U = T,
+			std::enable_if_t<std::is_integral_v<U>, U>* = nullptr>
+			constexpr Point<T>& operator/=(T divisor) {
+			if (divisor == 0) {
+				throw std::exception("Cannot divide by zero!");
+			}
+
+			if (divisor == 1) {
+				return *this;
+			}
+
+			mXPos /= divisor;
+			mYPos /= divisor;
+			return *this;
+		}
+
+		template <typename U = T,
+			std::enable_if_t<std::is_floating_point_v<U>, U>* = nullptr>
+			constexpr Point<T>& operator/=(T divisor) {
+			if (divisor == 0.0) {
+				throw std::exception("Cannot divide by zero!");
+			}
+
+			if (divisor == 1) {
+				return *this;
+			}
+
+			mXPos /= divisor;
+			mYPos /= divisor;
 			return *this;
 		}
 
 		template <typename U = T,
 			std::enable_if_t<std::is_integral_v<U>, U>* = nullptr>
-			bool operator==(const Point<U>& point) {
-			return ((*this - point) == 0);
+			constexpr bool operator==(const Point<U>& point) const {
+			return (mXPos - point.mXPos) == 0 && (mYPos - point.mYPos) == 0;
 		}
 
 		template <typename U = T,
 			std::enable_if_t<std::is_floating_point_v<U>, U>* = nullptr>
-			bool operator==(const Point<U>& point) {
+			constexpr bool operator==(const Point<U>& point) const {
 			return std::abs(this->getX() - point.getX()) < std::numeric_limits<U>::epsilon() &&
 				std::abs(this->getY() - point.getY()) < std::numeric_limits<U>::epsilon();
 		}
 
-	private:
-		friend std::ostream& operator<<(std::ostream& os, const Point<T>& point) {
-			return os << "Point: (" << point.m_xPos << "," << point.m_yPos << ")";
+		template <typename U = T,
+			std::enable_if_t<std::is_integral_v<U>, U>* = nullptr>
+			constexpr bool operator!=(const Point<U>& point) const {
+			return (mXPos - point.mXPos) != 0 || (mYPos - point.mYPos) != 0;
+		}
+
+		template <typename U = T,
+			std::enable_if_t<std::is_floating_point_v<U>, U>* = nullptr>
+			constexpr bool operator!=(const Point<U>& point) const {
+			return std::abs(this->getX() - point.getX()) > std::numeric_limits<U>::epsilon() ||
+				std::abs(this->getY() - point.getY()) > std::numeric_limits<U>::epsilon();
+		}
+
+		constexpr static T dotProduct(const Point<T>& p1, const Point<T>& p2) {
+			return p1.getX() * p2.getX() + p1.getY() * p2.getY();
 		}
 
 	private:
-		T m_xPos = {};
-		T m_yPos = {};
+		friend std::ostream& operator<<(std::ostream& os, const Point<T>& point) {
+			return os << "Point: (" << point.mXPos << "," << point.mYPos << ")";
+		}
+
+	private:
+		T mXPos = {};
+		T mYPos = {};
 	};
 } // namespace slohani
---
+++ unit-test/PointTest.cpp
@@ -0,0 +1,226 @@
+#include "gtest/gtest.h"
+#include "Point.h"
+
+TEST(PointTest, EmptyCtorTest)
+{
+	slohani::Point<int> p1;
+	EXPECT_TRUE(p1.isNull());
+
+	slohani::Point<float> p2;
+	EXPECT_TRUE(p2.isNull());
+}
+
+TEST(PointTest, ParameterizedCtorTest)
+{
+	slohani::Point<int> p1(5, 6);
+	EXPECT_EQ(5, p1.getX());
+	EXPECT_EQ(6, p1.getY());
+
+	slohani::Point<float> p2(5.6f, 6.5f);	
+	EXPECT_FLOAT_EQ(5.6f, p2.getX());
+	EXPECT_FLOAT_EQ(6.5f, p2.getY());
+}
+TEST(PointTest, IsNullTest) 
+{
+	slohani::Point<int> p1;
+	EXPECT_TRUE(p1.isNull());
+	EXPECT_EQ(0, p1.getX());
+	EXPECT_EQ(0, p1.getY());
+
+	slohani::Point<float> p2;
+	EXPECT_TRUE(p2.isNull());
+	EXPECT_EQ(0.f, p2.getX());
+	EXPECT_EQ(0.f, p2.getY());
+
+	slohani::Point<float> p3(0.f, 5.0f);
+	EXPECT_FALSE(p3.isNull());
+
+	slohani::Point<float> p4(0.f, 5.0f);
+	EXPECT_FALSE(p4.isNull());
+	
+	slohani::Point<float> p5(0.5f, 0.f);
+	EXPECT_FALSE(p5.isNull());
+
+	slohani::Point<float> p6(0.f, 0.f);
+	EXPECT_TRUE(p6.isNull());
+}
+
+TEST(PointTest, TestSetAndGet)
+{
+	slohani::Point<int> p1;
+	EXPECT_TRUE(p1.isNull());
+	
+	p1.setX(6);
+	p1.setY(7);
+	EXPECT_EQ(6, p1.getX());
+	EXPECT_EQ(7, p1.getY());
+	
+	slohani::Point<double> p2;
+	EXPECT_TRUE(p2.isNull());
+
+	p2.setX(6.789);
+	p2.setY(7.654);
+	EXPECT_EQ(6.789, p2.getX());
+	EXPECT_EQ(7.654, p2.getY());
+}
+
+TEST(PointTest, TestAddingPoints)
+{
+	slohani::Point<int> p1(9, 8);
+	slohani::Point<int> p2(7, 5);
+
+	auto p3 = p1 + p2;
+
+	EXPECT_EQ(9, p1.getX());
+	EXPECT_EQ(8, p1.getY());
+
+	EXPECT_EQ(16, p3.getX());
+	EXPECT_EQ(13, p3.getY());
+
+	auto p4 = p1 + p2 + p3;
+
+	EXPECT_EQ(32, p4.getX());
+	EXPECT_EQ(26, p4.getY());
+
+	p4 = p4 + 5;
+
+	EXPECT_EQ(37, p4.getX());
+	EXPECT_EQ(31, p4.getY());
+
+	p4 += 3;
+
+	EXPECT_EQ(40, p4.getX());
+	EXPECT_EQ(34, p4.getY());
+
+	p4 += slohani::Point<int>(5, 6);
+
+	EXPECT_EQ(9, p1.getX());
+	EXPECT_EQ(8, p1.getY());
+
+	EXPECT_EQ(45, p4.getX());
+	EXPECT_EQ(40, p4.getY());
+}
+
+TEST(PointTest, TestSubtractingPoints)
+{
+	slohani::Point<int> p1(9, 8);
+	slohani::Point<int> p2(7, 5);
+
+	auto p3 = p1 - p2;
+
+	EXPECT_EQ(9, p1.getX());
+	EXPECT_EQ(8, p1.getY());
+
+	EXPECT_EQ(2, p3.getX());
+	EXPECT_EQ(3, p3.getY());
+
+	auto p4 = p1 - p2 - p3;
+
+	EXPECT_TRUE(p4.isNull());
+
+	p4 = p4 - 5;
+
+	EXPECT_EQ(-5, p4.getX());
+	EXPECT_EQ(-5, p4.getY());
+
+	p4 -= 3;
+
+	EXPECT_EQ(-8, p4.getX());
+	EXPECT_EQ(-8, p4.getY());
+
+	p4 -= slohani::Point<int>(5, -6);
+
+	EXPECT_EQ(9, p1.getX());
+	EXPECT_EQ(8, p1.getY());
+
+	EXPECT_EQ(-13, p4.getX());
+	EXPECT_EQ(-2, p4.getY());
+}
+
+TEST(PointTest, TestMultiplyingPoints)
+{
+	slohani::Point<int> p1(9, 8);
+
+	auto p2 = p1 * 3;
+
+	EXPECT_EQ(9, p1.getX());
+	EXPECT_EQ(8, p1.getY());
+
+	EXPECT_EQ(27, p2.getX());
+	EXPECT_EQ(24, p2.getY());
+
+	p2 *= 5;
+
+	EXPECT_EQ(9, p1.getX());
+	EXPECT_EQ(8, p1.getY());
+
+	EXPECT_EQ(135, p2.getX());
+	EXPECT_EQ(120, p2.getY());
+}
+
+TEST(PointTest, TestDividingPoints)
+{
+	slohani::Point<int> p1(4, 8);
+
+	auto p2 = p1 / 2;
+
+	EXPECT_EQ(4, p1.getX());
+	EXPECT_EQ(8, p1.getY());
+
+	EXPECT_EQ(2, p2.getX());
+	EXPECT_EQ(4, p2.getY());
+
+	p2 /= 2;
+
+	EXPECT_EQ(4, p1.getX());
+	EXPECT_EQ(8, p1.getY());
+
+	EXPECT_EQ(1, p2.getX());
+	EXPECT_EQ(2, p2.getY());
+}
+
+TEST(PointTest, TestComparingPointsIsEqual)
+{
+	slohani::Point<int> p1(4, 8);
+	slohani::Point<int> p2(4, 8);
+
+	EXPECT_EQ(p1, p2);
+
+	slohani::Point<double> p3(4.56, 7.89);
+	slohani::Point<double> p4(4.56, 7.89);
+
+	EXPECT_EQ(p3, p4);
+}
+
+TEST(PointTest, TestComparingPointsIsUnEqual)
+{
+	slohani::Point<int> p1(4, 8);
+	slohani::Point<int> p2(4, 9);
+	slohani::Point<int> p3(5, 8);
+
+	EXPECT_NE(p1, p2);
+	EXPECT_NE(p1, p3);
+	EXPECT_NE(p2, p3);
+
+	slohani::Point<double> p4(4.56, 7.89);
+	slohani::Point<double> p5(4.56, 7.8);
+	slohani::Point<double> p6(4.5, 7.89);
+
+	EXPECT_NE(p4, p5);
+	EXPECT_NE(p5, p6);
+	EXPECT_NE(p6, p4);
+}
+
+TEST(PointTest, TestDotProduct)
+{
+	slohani::Point<double> p1(3.1, 7.1);
+	slohani::Point<double> p2(-1.0, 4.1);
+
+	EXPECT_DOUBLE_EQ(26.01, slohani::Point<double>::dotProduct(p1, p2));
+}
+
+
+
+
+
+
