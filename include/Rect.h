808186f34c437172b7cdac7c157ecda0af153837
Sonu Lohani <sonulohani@gmail.com>
2018-09-19 20:45:58 +0530

    Added Rect class and its unit test

--- include/Rect.h
+++ include/Rect.h
@@ -2,6 +2,8 @@
 
 #include "Point.h"
 
+#include <type_traits>
+
 namespace slohani {
 	template<typename T>
 	class Rect {
@@ -9,28 +11,91 @@ namespace slohani {
 		explicit constexpr Rect() = default;
 
 		explicit constexpr Rect(const Point<T>& topLeft, const Point<T>& bottomRight) {
-			mX1 = topLeft.getX();
-			mY1 = topLeft.getY();
-			mX2 = bottomRight.getX();
-			mY2 = bottomRight.getY();
+			mX = topLeft.getX();
+			mY = topLeft.getY();
+			mWidth = bottomRight.getX() - topLeft.getX();
+			mWidth = bottomRight.getY() - topLeft.getY();
 		}
 
 		explicit constexpr Rect(T x, T y, T width, T height) {
-			mX1 = x;
-			mY1 = y;
-			mX2 = mX1 + width - 1;
-			mY2 = mY1 + height - 1;
+			mX = x;
+			mY = y;
+			mWidth = width;
+			mHeight = height;
 		}
 
 		// TBD
 		//Rect(const QPoint &topLeft, const Size &size)
 
+		template <typename U = T,
+			std::enable_if_t<std::is_integral_v<U>, U>* = nullptr>
 		constexpr inline bool isNull() const {
-			return mX2 == mX1 - 1 && mY2 == mY1 - 1;
+			return mWidth == 0 && mHeight == 0;
+		}
+
+		template <typename U = T,
+			std::enable_if_t<std::is_floating_point_v<U>, U>* = nullptr>
+		constexpr inline bool isNull() const {
+			return mWidth == 0.0 && mHeight == 0.0;
+		}
+
+		template <typename U = T,
+			std::enable_if_t<std::is_integral_v<U>, U>* = nullptr>
+		constexpr inline bool isValid() const {
+			return mWidth > 0 && mHeight > 0;
+		}
+
+		template <typename U = T,
+			std::enable_if_t<std::is_floating_point_v<U>, U>* = nullptr>
+		constexpr inline bool isValid() const {
+			return mWidth > 0.0 && mHeight > 0.0;
+		}
+
+		template <typename U = T,
+			std::enable_if_t<std::is_integral_v<U>, U>* = nullptr>
+		constexpr inline bool isEmpty() const {
+			return !isValid<U>();
+		}
+
+		template <typename U = T,
+			std::enable_if_t<std::is_floating_point_v<U>, U>* = nullptr>
+		constexpr inline bool isEmpty() const {
+			return !isValid<U>();
+		}
+
+		constexpr inline T getWidth() const {
+			return mWidth;
+		}
+
+		constexpr inline T getHeight() const {
+			return mHeight;
+		}
+
+		constexpr inline T getX() const {
+			return mX;
+		}
+
+		constexpr inline T getT() const {
+			return mY;
+		}
+
+		constexpr inline Point<T> center() const {
+			return Point<T>(mX + mWidth/2, mY + mHeight/2);
+		}
+
+	private:
+		friend std::ostream& operator<<(std::ostream& os, const Rect<T>& rect) {
+			return os << "Rect: ( x : " << rect.getX() << ", y : " << 
+			rect.getX() << ", width : " << rect.getHeight() << 
+			", height: " << rect.getHeight() << ")";
 		}
 
 
+		
 	private:
-		T mX1, mY1, mX2, mY2;
+		T mX = {}; 
+		T mY = {};
+		T mWidth = {};
+		T mHeight = {};
 	};
 }
---
+++ unit-test/RectTest.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+#include "Rect.h"
+
+TEST(RectTest, EmptyCtorTest)
+{
+    slohani::Rect<int> rect1;
+    EXPECT_TRUE(rect1.isNull());
+    EXPECT_TRUE(rect1.isEmpty());
+    EXPECT_FALSE(rect1.isValid());
+
+    slohani::Rect<double> rect2;
+    EXPECT_TRUE(rect2.isNull());
+    EXPECT_TRUE(rect2.isEmpty());
+    EXPECT_FALSE(rect2.isValid());
+}
